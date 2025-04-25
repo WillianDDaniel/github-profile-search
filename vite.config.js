ecbf5101d8a218facbfc7363a277a763b5fc537f
Willian Daniel <williandeivitidaniel@live.com>
2025-04-24 22:40:32 -0300

    Fix: Changed base path in vite.config.js to '/avanti-react-find-profiles/' and Build for github pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/avanti-react-find-profiles/',
   plugins: [
     tailwindcss(),
     react(),
