7ccb2d84f66eb9b7c73c609550846d003273b590
Willian Daniel <williandeivitidaniel@live.com>
2025-04-24 22:33:25 -0300

    Add search icon and update HTML structure for GitHub profile search
    - Added a new SVG search icon to the assets directory.
    - Created a new index.html file for the application with proper meta tags and links to stylesheets and scripts.
    - Updated the existing index.html to use the new search icon and changed the language to Portuguese.
    - Modified the UserNotFound component to improve text alignment and added a line break for better readability.

--- src/components/UserNotFound.jsx
+++ src/components/UserNotFound.jsx
@@ -1,11 +1,12 @@
 export default function UserNotFound() {
   return (
     <p
-      className="text-red-600 flex items-center justify-center
+      className="text-red-600 flex items-center justify-center text-center
       bg-[#D9D9D9] w-8/12 h-3/12 mt-3 rounded-2xl scale-105
       "
     >
       Nenhum perfil foi encontrado com ese nome de usuário.
+      <br />
       Tente novamente
     </p>
   )
